f367315b72f312fe23583e8d68e2825ef2a44add
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-06-22 20:47:44 -0400

    update

--- src/test/java/com/ebrahim/hossain/utilities/GetScreenShot.java
+++ src/test/java/com/ebrahim/hossain/utilities/GetScreenShot.java
@@ -2,19 +2,33 @@ package com.ebrahim.hossain.utilities;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+//Define a public class named GetScreenShot
 public class GetScreenShot {
+
+	// Define a public static method named capture that takes a WebDriver instance and a screenshot name as parameters
+	// This method returns the path of the captured screenshot as a String and throws IOException
 	public static String capture(WebDriver driver, String screenShotName) throws IOException {
+		// Cast the WebDriver instance to TakesScreenshot to enable screenshot capturing
 		TakesScreenshot ts = (TakesScreenshot) driver;
+
+		// Capture the screenshot and store it as a File object
 		File source = ts.getScreenshotAs(OutputType.FILE);
-		String dest = System.getProperty("user.dir")+"\\screenshots\\"+screenShotName+".png";
+
+		// Construct the destination path for the screenshot file
+		String dest = System.getProperty("user.dir") + "\\screenshots\\" + screenShotName + ".png";
+
+		// Create a new File object for the destination path
 		File destination = new File(dest);
+
+		// Copy the screenshot file to the destination path
 		FileUtils.copyFile(source, destination);
-		return dest;	
+
+		// Return the destination path of the screenshot
+		return dest;
 	}
 }
